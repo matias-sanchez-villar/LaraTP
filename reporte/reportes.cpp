bd4b83e982afca2dcb6e69cb7b0a34a9820732ce
Matias <matias.sanchez@alumnos.frgp.utn.edu.ar>
2020-12-03 20:08:02 -0300

    punto 1

--- reporte/reportes.cpp
+++ reporte/reportes.cpp
@@ -15,19 +15,40 @@ using namespace std;
 #include "../usuarios/usuario.h"
 
 /*
-
+    * A partir de una altura y peso que se ingresen por teclado, mostrar todos
+    * los usuarios que tengan altura mayor a la ingresa o peso mayor al
+    * ingresado.
 */
 
 void reporte1(){
     title("REPORTE 1", APP_TITLEFORECOLOR, APP_TITLEBACKCOLOR);
-
+    float altura, peso;
+    int pos=0;
+    bool estado=false;
+    usuario uno;
+    cout<<endl<<"Ingrese la altura: ";
+    cin>>altura;
+    cout<<"Ingrese peso: ";
+    cin>>peso;
+    while(LeerUsuario(uno, pos++)){
+        if(altura<uno.altura && peso<uno.peso){
+            MostrarUsuario(uno);
+            estado=true;
+        }
+    }
+    if(estado==false){
+        system("cls");
+        msj("NO EXISTE USUARIO CON ESAS CONDICIONES", 15, 3, 1, 1);
+        return;
+    }
 
     system("pause");
     system("cls");
 }
 
 /*
-
+    *Listar la cantidad de usuarios distintos que hayan entrenado
+    *más de 250 minutos en algún registro de entrenamiento.
 */
 
 void reporte2(){
