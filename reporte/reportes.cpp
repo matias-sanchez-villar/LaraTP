3fa42b49233e586359c9fc5a01ef90d7f383c824
Matias <matias.sanchez@alumnos.frgp.utn.edu.ar>
2021-02-18 00:06:49 -0300

    mini cambios para rendir mañana

--- reporte/reportes.cpp
+++ reporte/reportes.cpp
@@ -15,30 +15,12 @@ using namespace std;
 #include "../usuarios/usuario.h"
 
 /*
-    *A partir de un IDUsuario que se ingresa por teclado, listar el entrenamiento
-    *de mayor cantidad de calorías y la fecha en que las registró.
-    *Si hay dos registro con misma cantidad, mostrar el primero de ellos.
+
 */
 
 void reporte1(){
     title("REPORTE 1", APP_TITLEFORECOLOR, APP_TITLEBACKCOLOR);cout<<endl;
 
-    int ID, pos=0;
-    float Calorias=0;
-    entrenamiento uno;
-    cout<<"Ingrese el ID de Usuario: ";
-    cin>>ID;
-    for(int x=0;x<CantidadEntrenaminto();x++){
-        LeerEntrenamiento(uno, x);
-        if(uno.idUsuario==ID && uno.calorias>Calorias){
-            pos=x;
-            cout<<pos;
-            Calorias=uno.calorias;
-            cout<<Calorias;
-        }
-    }
-    LeerEntrenamiento(uno, pos);
-    MostrarEntramiento(uno);
 
     system("pause");
     system("cls");
@@ -50,6 +32,8 @@ void reporte1(){
 
 void reporte2(){
     title("REPORTE 2", APP_TITLEFORECOLOR, APP_TITLEBACKCOLOR);cout<<endl;
+
+
 
     system("pause");
     system("cls");
